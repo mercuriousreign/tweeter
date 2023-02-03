dac4e78b226824b38b3d6be646a60128e8f6b9ba
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-02 21:39:28 -0500

    Format javascript file and css files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,20 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$( document ).ready(()=>{
+$(document).ready(()=>{
   const area = document.getElementById("tweet-text");
   const errorlog = document.getElementById('error-log');
   $(errorlog).slideToggle();
 
   /**Method to use to negate cross site scripting */
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
   
 
-/**Creates the tweet element and adds it to the user page*/
+  /**Creates the tweet element and adds it to the user page*/
   const createTweetElement  = function(tweet) {
     let date = timeago.format(tweet.created_at);
     const tweetTemplate = `<section class="tweet-container">
@@ -37,18 +37,18 @@ $( document ).ready(()=>{
           <button type="submit"><i class="fa-solid fa-heart"></i></button>
         </div>
       </div>
-    </section>`
+    </section>`;
   
-    $('.tweetline').append(tweetTemplate)
-  }
+    $('.tweetline').append(tweetTemplate);
+  };
   
   /**Add whole list of tweet to be showcased  */
   const renderTweets = function(tweets) {
     area.value = "";
     for (const tweet of tweets) {
-      createTweetElement(tweet)
+      createTweetElement(tweet);
     }
-  }
+  };
   
 
   /**Gets the list of tweet from database*/
@@ -57,42 +57,42 @@ $( document ).ready(()=>{
       url: `/tweets`,
       method: 'GET',
       dataType: 'JSON'
-  }).then(function(response) {
+    }).then(function(response) {
       $('.tweetline').empty();
       renderTweets(response);
-  })
+    });
 
-  }
+  };
 
   /**Allows user to submit their tweet, shows an error if it does not fit into requirement */
   $('#tweetsend').submit(function(event) {
     event.preventDefault();
     if (!area.value) {
-      errorlog.innerHTML= "Please input a message to tweet";
+      errorlog.innerHTML = "Please input a message to tweet";
       return $(errorlog).slideDown();
     }
     if (area.textLength > 140) {
-      errorlog.innerHTML= "Message must be under 140 character to post";
-      return $(errorlog).slideDown(); 
+      errorlog.innerHTML = "Message must be under 140 character to post";
+      return $(errorlog).slideDown();
     }
 
     /**Hides the error bar if it is already there but there is no more error */
-    if(errorlog.innerHTML){
-      errorlog.innerHTML="";
-      $(errorlog).slideUp(); 
+    if (errorlog.innerHTML) {
+      errorlog.innerHTML = "";
+      $(errorlog).slideUp();
 
     }
 
     $.ajax({
       url:"/tweets",
       method:"POST",
-      data : $( this ).serialize(), 
+      data : $(this).serialize(),
       success : (ev) => {
         loadTweets();
       }
-  })
+    });
 
-})
-/** Loads all the tweets first when the user first land on the page */
-loadTweets();
-  })
+  });
+  /** Loads all the tweets first when the user first land on the page */
+  loadTweets();
+});
